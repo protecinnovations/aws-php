f21a5a648121c8723011141f632f18523bd1336f
Martin Meredith <martinmeredith@protecinnovations.co.uk>
2012-05-28 12:01:53 +0100

    pyrus doesn't use all-deps, but uses optional deps

--- ci/dependencies.sh
+++ ci/dependencies.sh
@@ -2,6 +2,6 @@
 
 pyrus channel-discover pear.survivethedeepend.com
 pyrus channel-discover hamcrest.googlecode.com/svn/pear
-pyrus install deepend/Mockery --alldeps
+pyrus install deepend/Mockery -o
 
 phpenv rehash
