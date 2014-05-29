78d4f48415fd307758f78ba1660aaaf936505d34
Martin Meredith <martinmeredith@protecinnovations.co.uk>
2014-05-29 14:26:43 +0100

    Update so travis builds work

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,7 +1,6 @@
 <?php
+require_once __DIR__ . '/../vendor/autoload.php';
 
-require_once 'Mockery/Loader.php';
-//require_once 'Hamcrest/Hamcrest.php';
 $loader = new \Mockery\Loader;
 $loader->register();
 
